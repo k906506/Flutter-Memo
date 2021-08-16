6f8a0afab380703c0494d71da62495b9e672a79d
k906506 <k906506@naver.com>
2021-08-16 11:06:16 +0900

    삭제된 메모 관련 Provider 추가

--- lib/providers/memo_provider.dart
+++ lib/providers/memo_provider.dart
@@ -3,33 +3,65 @@ import 'package:fluttermemo/models/memo.dart';
 
 class MemoProvider with ChangeNotifier {
   List<Memo> memos = [];
+  List<Memo> deletedMemos = [];
 
   List<Memo> get items {
     return [...memos];
   }
 
+  List<Memo> get deletedItems {
+    return [...deletedMemos];
+  }
+
   // Future<void> fetchAndSetMemo() {
   // }
 
-  Future<void> addMemo(String title, String content) async {
-    memos.add(new Memo(DateTime.now().toString(), title, content, DateTime.now()));
+  // 메모 리스트에 추가하는 메소드
+  Future<void> addMemo(String id, String title, String content) async {
+    if (id == "") {
+      memos.add(
+        new Memo(
+          DateTime.now().toString(),
+          title,
+          content,
+          DateTime.now(),
+        ),
+      );
+    } else {
+      var index = memos.indexWhere((element) => element.id == id);
+      memos[index] = new Memo(
+        id,
+        title,
+        content,
+        DateTime.now(),
+      );
+    }
     sortItems();
     notifyListeners();
   }
 
+  // 메모를 제거할 때 제거된 메모 리스트에 추가하는 메소드
+  Future<void> addDeletedMemo(
+      String id, String title, String content, DateTime uploadDate) async {
+    deletedMemos.add(
+      new Memo(
+        id,
+        title,
+        content,
+        uploadDate,
+      ),
+    );
+    notifyListeners();
+  }
+
+  // 메모를 제거하는 메소드
   Future<void> deleteMemo(String id) async {
     memos.removeWhere((element) => element.id == id);
     sortItems();
     notifyListeners();
   }
 
-  Future<void> updateMemo(String id, String title, String content) async {
-    var index = memos.indexWhere((element) => element.id == id);
-    memos[index] = new Memo(id, title, content, DateTime.now());
-    sortItems();
-    notifyListeners();
-  }
-
+  // 메모 생정(수정) 일자를 기준으로 내림차순으로 정렬하는 메소드
   void sortItems() {
     memos.sort((a, b) => b.uploadDate.compareTo(a.uploadDate));
   }
