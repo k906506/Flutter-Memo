76fc0952678c5dc63f152a0873ee6fde9f957deb
k906506 <k906506@naver.com>
2021-08-16 11:02:24 +0900

    Stateful로 변경 후 데이터 저장

--- lib/screens/add_memo_screen.dart
+++ lib/screens/add_memo_screen.dart
@@ -3,10 +3,14 @@ import 'package:provider/provider.dart';
 import '/providers/memo_provider.dart';
 import '/models/memo.dart';
 
-
-class AddMemoScreen extends StatelessWidget {
+class AddMemoScreen extends StatefulWidget {
   static const routeName = '/add-memo';
 
+  @override
+  _AddMemoScreenState createState() => _AddMemoScreenState();
+}
+
+class _AddMemoScreenState extends State<AddMemoScreen> {
   final formKey = GlobalKey<FormState>();
 
   Memo _addMemo = new Memo(
@@ -20,7 +24,8 @@ class AddMemoScreen extends StatelessWidget {
     final isValid = formKey.currentState!.validate();
     if (isValid) {
       formKey.currentState!.save();
-      Provider.of<MemoProvider>(context,listen: false).addMemo(_addMemo.title, _addMemo.content);
+      Provider.of<MemoProvider>(context, listen: false)
+          .addMemo(_addMemo.id, _addMemo.title, _addMemo.content);
       Navigator.of(context).pop();
     } else {
       // Scaffold.of(context).showSnackBar(
@@ -33,9 +38,18 @@ class AddMemoScreen extends StatelessWidget {
   }
 
   @override
+  void didChangeDependencies() {
+    final arguments = ModalRoute.of(context)!.settings.arguments as Memo;
+    _addMemo.id = arguments.id;
+    _addMemo.title = arguments.title;
+    _addMemo.content = arguments.content;
+    super.didChangeDependencies();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
+      // resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: Text("메모 등록"),
         actions: <Widget>[
@@ -47,96 +61,100 @@ class AddMemoScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                child: Text(
-                  "제목",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  child: Text(
+                    "제목",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Container(
-                child: Column(
-                  children: <Widget>[
-                    TextFormField(
-                      decoration: InputDecoration(labelText: "제목을 입력해주세요."),
-                      textInputAction: TextInputAction.next,
-                      onSaved: (value) {
-                        _addMemo = Memo(_addMemo.id, value.toString(), _addMemo.content,
-                            _addMemo.uploadDate);
-                      },
-                      validator: (value) {
-                        if (value.toString().isEmpty) {
-                          return "제목은 필수사항입니다.";
-                        }
-                      },
-                    ),
-                  ],
+                SizedBox(
+                  height: 5,
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                child: Text(
-                  "내용",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
+                Container(
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                        decoration: InputDecoration(labelText: "제목을 입력해주세요."),
+                        textInputAction: TextInputAction.next,
+                        onSaved: (value) {
+                          _addMemo = Memo(_addMemo.id, value.toString(),
+                              _addMemo.content, _addMemo.uploadDate);
+                        },
+                        validator: (value) {
+                          if (value.toString().isEmpty) {
+                            return "제목은 필수사항입니다.";
+                          }
+                        },
+                        initialValue: _addMemo.title,
+                      ),
+                    ],
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                      height: MediaQuery.of(context).size.height * 0.3,
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.grey,
-                          width: 0.7,
-                        ),
-                      ),
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        padding: const EdgeInsets.all(8),
-                        child: TextFormField(
-                          decoration: InputDecoration(
-                              labelText: "내용을 입력해주세요.",
-                              border: InputBorder.none),
-                          maxLines: null,
-                          textInputAction: TextInputAction.done,
-                          keyboardType: TextInputType.multiline,
-                          onSaved: (value) {
-                            _addMemo = Memo(_addMemo.id, _addMemo.title, value.toString(),
-                                _addMemo.uploadDate);
-                          },
-                          validator: (value) {
-                            if (value.toString().isEmpty) {
-                              return "내용은 필수사항입니다.";
-                            }
-                          },
-                        ),
-                      ),
-                    ),
-                  ],
+                SizedBox(
+                  height: 20,
                 ),
-              ),
-            ],
+                Container(
+                  child: Text(
+                    "내용",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.6,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.grey,
+                            width: 0.7,
+                          ),
+                        ),
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          padding: const EdgeInsets.all(8),
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                                labelText: "내용을 입력해주세요.",
+                                border: InputBorder.none),
+                            maxLines: null,
+                            textInputAction: TextInputAction.done,
+                            keyboardType: TextInputType.multiline,
+                            onSaved: (value) {
+                              _addMemo = Memo(_addMemo.id, _addMemo.title,
+                                  value.toString(), _addMemo.uploadDate);
+                            },
+                            validator: (value) {
+                              if (value.toString().isEmpty) {
+                                return "내용은 필수사항입니다.";
+                              }
+                            },
+                            initialValue: _addMemo.content,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
